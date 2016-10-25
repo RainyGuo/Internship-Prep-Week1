221e3a528e37f612149914dee2a111e920bcfbbd
Siraj Zaneer <szaneer@gmail.com>
2016-10-25 16:56:13 -0400

    5

--- src/Challenge4.java
+++ src/Challenge4.java
@@ -3,6 +3,19 @@
  */
 public class Challenge4 {
     public boolean palindromeDetector(String text) {
-        
+        String cleaned = text.replaceAll("[^A-Za-z0-9]", "");
+
+        int fromStart = 0;
+        int fromEnd = text.length() - 1;
+
+        while (fromEnd > fromStart) {
+            if (text.charAt(fromStart) != text.charAt(fromEnd)) {
+                return false;
+            }
+
+            fromStart++;
+            fromEnd--;
+        }
+        return true;
     }
 }
